9c3c171dfb2cd6ea8f3db6ac8bb5bebec9d2e3c5
VQ <you@example.com>
2024-05-27 12:46:27 -0400

    Crear depositos para monedas y productos en Comprador

--- src/main/java/t3/logica_expendedor/Comprador.java
+++ src/main/java/t3/logica_expendedor/Comprador.java
@@ -1,20 +1,39 @@
 package t3.logica_expendedor;
 
+import t3.logica_expendedor.Bebidas.*;
+import t3.logica_expendedor.Dulces.*;
 import t3.logica_expendedor.Monedas.*;
 import t3.logica_expendedor.Producto;
 import t3.logica_expendedor.Excepciones.*;
 import t3.logica_expendedor.Precios_Productos;
 
+import javax.swing.*;
+
 public class Comprador{
     //DECLARACIONES DE ATRIBUTOS
     private String sonido;  //Guarda el sonido/gesto de consumir el producto comprado
     private int vuelto = 0; //Guarda el valor numeroco (int) del vuelto resibido al comprar el producto
+    private Expendedor expendedor;
+
+    // depositos para monedas del comprador
+    private Deposito<Moneda100> depositoMoneda100;
+    private Deposito<Moneda500> depositoMoneda500;
+    private Deposito<Moneda1000> depositoMoneda1000;
+    private Deposito<Moneda1500> depositoMoneda1500;
+
+    // depositos para productos del comprador
+    private Deposito<CocaCola> depositoCocacola;
+    private Deposito<Sprite> depositoSprite;
+    private Deposito<Fanta> depositoFanta;
+    private Deposito<Snickers> depositoSnickers;
+    private Deposito<Super8> depositoSuper8;
 
     //DECLARACIONES DE METODOS
-    //Constructor: recibe la moneda con la que comprara, un numero que identifica el tipo de producto y la referencia al Expendedor en el que comprara
 
+    //Constructor: recibe la moneda con la que comprara, un numero que identifica el tipo de producto y la referencia al Expendedor en el que comprara
     public Comprador(Moneda moneda, Precios_Productos cualProducto, Expendedor exp) throws PagoInsuficienteException, PagoIncorrectoException, NoHayProductoException{
 
+        expendedor = exp;
         Producto producto = null;
 
         try{
@@ -40,8 +59,26 @@ public class Comprador{
             sonido = null;          //El sonido de consumir el producto queda como null
 
         } else {sonido = producto.consumir();}  //Si se logro consumir el producto se asigna el gesto/sonido de consumir el producto
+
+
+
+        // incializar depositos
+
+        depositoMoneda100 = new Deposito<Moneda100>();
+        depositoMoneda500 = new Deposito<Moneda500>();
+        depositoMoneda1000 = new Deposito<Moneda1000>();
+        depositoMoneda1500 = new Deposito<Moneda1500>();
+
+        depositoCocacola = new Deposito<CocaCola>();
+        depositoSprite = new Deposito<Sprite>();
+        depositoFanta = new Deposito<Fanta>();
+        depositoSuper8 = new Deposito<Super8>();
+        depositoSnickers = new Deposito<Snickers>();
     }
 
     public int cuantoVuelto() {return vuelto;}      //Funcion que retorna el vuelto como un valor "int"
     public String queConsumiste() {return sonido;}  //Funcion que retorna el sonido de consumir el producto comprado en formato "String"
+
+
+
 }
