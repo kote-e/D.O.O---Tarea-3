22c473ff7cd1e9adc50ca139cce596c065e83651
kote-e <mariasanmartin2023@udec.cl>
2024-05-29 16:46:00 -0400

    Cambios en el javadoc de las Excepciones

--- src/main/java/t3/logica_expendedor/Excepciones/PagoIncorrectoException.java
+++ src/main/java/t3/logica_expendedor/Excepciones/PagoIncorrectoException.java
@@ -1,6 +1,15 @@
 package t3.logica_expendedor.Excepciones;
-
+/**
+ * PagoIncorrectoException es una subclase de Exception que ocurre
+ * cuando no se ha ingresado ninguna moneda para realizar el pago
+ * @author
+ */
 public class PagoIncorrectoException extends Exception{
+    /**
+     * Se le asigna un mensaje a la excepción que luego se mostrara en la terminal
+     * por el canal de error
+     * @param message mensaje que se mostrara
+     */
     public PagoIncorrectoException (String message){
         super(message);
     }
