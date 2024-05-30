dff4ad6525312f81dc2447e0dee48d59c6b188b4
kote-e <mariasanmartin2023@udec.cl>
2024-05-29 20:14:10 -0400

    Cambios en el javadoc de la clase Deposito

--- src/main/java/t3/logica_expendedor/Deposito.java
+++ src/main/java/t3/logica_expendedor/Deposito.java
@@ -3,35 +3,35 @@ package t3.logica_expendedor;
 import java.util.ArrayList;
 
 /**
- * Clase que representa a un Deposito de Productos o Monedas
- * @param <T>   //Tipo de Productos o Monedas que almacena
+ * Clase que representa a un Depósito de Productos o Monedas
+ * @param <T>  Tipo de Productos o Monedas que almacena
  */
 public class Deposito<T>{
     ArrayList<T> list; //Definicion de la lista que guardara a las productos
 
     /**
-     * Instanciacion de la lista que guardara a las productos
+     * Instanciación de la lista que guardara a los productos o las monedas
      */
     public Deposito() {list = new ArrayList<T>();}
 
     /**
-     * Agregar un Producto o Moneda
+     * Agregar un Producto o Moneda al deposito
      * @param x Producto o Moneda a ingresar
      */
     public void add(T x) {list.add(x);}
 
     /**
      * Funcion para retirar Productos o monedas de la lista
-     * @return Producto o Moneda a retirar
+     * @return Producto o Moneda a retirar, o null si el depósito está vacío
      */
     public T get(){
-        if (list.size() != 0) {return list.remove(0);}  //Si la lista no esta vacia se retorna al primera producto de la lista eliminadolo a la ves de esta lista
-        else return null;       //Si la lista esta vacia se retorna null
+        if (list.size() != 0) {return list.remove(0);}  //Si la lista no está vacia se retorna el primer producto de la lista eliminadolo a la ves de esta lista
+        else return null;       //Si la lista esta vacía se retorna null
     }
 
     /**
-     * Funcion que devuelve la cantidad de Productos o Monedas que contiene
-     * @return
+     * Funcion que devuelve la cantidad de Productos o Monedas que contiene el depósito
+     * @return la cantidad de productos o monedas del depósito
      */
     public int size(){return list.size();}
 }
