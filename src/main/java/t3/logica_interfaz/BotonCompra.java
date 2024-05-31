c46984e8414334feba616aea731015791b631199
kote-e <mariasanmartin2023@udec.cl>
2024-05-31 00:28:26 -0400

    Cambios en el javadoc de la clase BotonCompra

--- src/main/java/t3/logica_interfaz/BotonCompra.java
+++ src/main/java/t3/logica_interfaz/BotonCompra.java
@@ -8,9 +8,23 @@ import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
+/**
+ * La subclase BotonCompra es una extensión de JLabel e implementa MouseListener y representa
+ * un boton en la interfaz para realizar una compra al ser precionado
+ * @see PanelExpendedor
+ * @see Expendedor
+ */
 public class BotonCompra extends JLabel implements MouseListener{
     private final PanelExpendedor pExp;
 
+    /**
+     *  crea una instancia de BoonbCompra
+     * @param pExp El panel del expendedor asociado con este botón
+     * @param x esta es la cooredenada x donde se encuentrab el bóton en la pantalla
+     * @param y esta es la cooredenada y donde se encuentrab el bóton en la pantalla
+     * @param width este es el ancho del boton
+     * @param height este es el alto del boton
+     */
     public BotonCompra(PanelExpendedor pExp, int x, int y, int width, int height) {
         this.pExp = pExp;
         this.setBounds(x,y,width,height);
@@ -18,8 +32,18 @@ public class BotonCompra extends JLabel implements MouseListener{
         this.addMouseListener(this);
     }
 
-    public void mouseClicked(MouseEvent me) {;} // es llamado cuando el press y el release ocurren en el mismo pixel
+    /**
+     * este metodo se utiliza al hacer clic en el boton (press y el release
+     * ocurren en el mismo pixel)
+     * @param me este es el evento del mouse
+     */
+    public void mouseClicked(MouseEvent me) {;}
 
+    /**
+     * este metodo se utiliza al precionar el botón del mouse se intenta realizar una compra
+     * y si algo sale mal ale un mensaje de error.
+     * @param me este es el evento del mouse
+     */
     public void mousePressed(MouseEvent me) {
         try{
             pExp.getComprador().comprar(pExp.getProducto());
@@ -32,9 +56,21 @@ public class BotonCompra extends JLabel implements MouseListener{
         }
     }
 
+    /**
+     * este metodo es cuando se deja de hacer clic en el mouse
+     * @param me este es el evento del mouse
+     */
     public void mouseReleased(MouseEvent me) {;}
 
+    /**
+     *esto se utiluza cuando el mouse esta dentro del area del botón
+     * @param me este es el evento del mouse
+     */
     public void mouseEntered(MouseEvent me) {;}
 
+    /**
+     * esto se utiliza cuando el mouse sale del del area del boton
+     * @param me es el evento del mouse
+     */
     public void mouseExited(MouseEvent me) {;}
 }
