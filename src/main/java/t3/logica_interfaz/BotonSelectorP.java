e7f0818b56d5a04ec4a44f1e811199845b5829d3
kote-e <mariasanmartin2023@udec.cl>
2024-05-31 02:18:52 -0400

    Cambios en el javadoc de la clase BotonSelector

--- src/main/java/t3/logica_interfaz/BotonSelectorP.java
+++ src/main/java/t3/logica_interfaz/BotonSelectorP.java
@@ -7,10 +7,24 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import javax.swing.*;
 
+/**
+ *  La subclase BotonSelectorP s una extensión de JLabel e implementa MouseListener
+ *  que representa un botón que muestra un producto
+ * @author
+ */
 public class BotonSelectorP extends JLabel implements MouseListener {
     private final Precios_Productos producto;
     private final PanelExpendedor pExpendedor;
 
+    /**
+     * Constructor para inicializar el botón con un producto
+     * @param pExp es el panel en expendedor asociado al boton
+     * @param producto El producto asociado a cada botón
+     * @param x  esta es la cooredenada x donde se encuentrab el bóton en la pantalla
+     * @param y esta es la cooredenada y donde se encuentrab el bóton en la pantalla
+     * @param width este es el ancho del boton
+     * @param height este es el alto del boton
+     */
     public BotonSelectorP(PanelExpendedor pExp, Precios_Productos producto, int x, int y, int width, int height) {
         this.pExpendedor = pExp;
         this.producto = producto;
@@ -20,20 +34,41 @@ public class BotonSelectorP extends JLabel implements MouseListener {
         this.addMouseListener(this);
     }
 
-    public void mouseClicked(MouseEvent me) {;} // es llamado cuando el press y el release ocurren en el mismo pixel
+    /**
+     *este metodo se utiliza al hacer clic en el boton (press y el release
+     *ocurren en el mismo pixel)
+     * @param me este es el evento del mouse
+     */
+    public void mouseClicked(MouseEvent me) {;}
 
+    /**
+     * este metodo se utiliza al precionar el botón del mouse el producto seleccionado en
+     * el panel expendedor e imprime el precio del producto seleccionado en la pantalla
+     * @param me este es el evento del mouse
+     */
     public void mousePressed(MouseEvent me) {
         pExpendedor.setProducto(producto);
         pExpendedor.getLetrero().ImprimirPrecio();
     }
-
+    /**
+     * este metodo es cuando se deja de hacer clic en el mouse
+     * @param me este es el evento del mouse
+     */
     public void mouseReleased(MouseEvent me) {;}
 
+    /**
+     * Cambia el color del botón cuando el mouse interactua con este
+     * @param me este es el evento del mouse
+     */
     public void mouseEntered(MouseEvent me) {
         BotonSelectorP lbl = (BotonSelectorP) me.getComponent();
         lbl.setBackground(new Color(0xDFC3FF));
     }
 
+    /**
+     * Restablece el color del botón cuando el mouse interactua con este
+     * @param me este es el evento del mouse
+     */
     public void mouseExited(MouseEvent me) {
         BotonSelectorP lbl = (BotonSelectorP) me.getComponent();
         lbl.setBackground(new Color(0xF4EAFF));
