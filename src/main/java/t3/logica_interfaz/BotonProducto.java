095bc2f1564cf3f77a4549445ab19de641734b20
kote-e <mariasanmartin2023@udec.cl>
2024-06-04 00:42:07 -0400

    cambios en el javadoc de la clase BotonProducto

--- src/main/java/t3/logica_interfaz/BotonProducto.java
+++ src/main/java/t3/logica_interfaz/BotonProducto.java
@@ -8,12 +8,28 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import javax.sound.sampled.Clip;
 
+/**
+ * La subclase BotonProducto es una extensión de JLabel e implementa MouseListener,
+ *GeneradorImagen y Sonidos.
+ * Representa un botón en la interfaz para sacar un producto al ser presionado
+ * @see PanelExpendedor
+ * @see PanelComprador
+ */
 public class BotonProducto extends JLabel implements MouseListener, GeneradorImagen, Sonidos {
     private final PanelExpendedor pExp;
     private PanelComprador panelComprador;
     private JLabel labelProducto = null;
     private final Clip abrirSonido;
 
+    /**
+     * Crea una instancia de BotonProducto
+     * @param pExp El panel el expendedor asociado con el botón
+     * @param panelComprador el panel del comprador asociado con este botón
+     * @param x esta es la coordenada x donde se encuentra el botón en la pantall
+     * @param y esta es la coordenada y donde se encuentra el botón en la pantalla
+     * @param width este es el ancho del botón
+     * @param height este es el alto del botón
+     */
     public BotonProducto(PanelExpendedor pExp,PanelComprador panelComprador ,int x, int y, int width, int height) {
         this.pExp = pExp;
         this.panelComprador = panelComprador;
@@ -26,8 +42,18 @@ public class BotonProducto extends JLabel implements MouseListener, GeneradorIma
         abrirSonido = Sonidos.cargarSonido("src/main/java/t3/logica_interfaz/Sonidos/Abrir_Compuerta.wav");
     }
 
+    /**
+     * Este emtodo se utiliza al hacer el clic en el botón (press y release
+     *ocurren en el mismo pixel)
+     * @param me este es el evento del mouse
+     */
     public void mouseClicked(MouseEvent me) {;}
 
+    /**
+     * Este metodo se utiliza al presionar el botón del mouse, intenta sacar un producto
+     * y si algo sale mal, muestra un mensaje de error
+     * @param me este es el evento del mouse
+     */
     public void mousePressed(MouseEvent me) {
         try{
             Producto prod = pExp.getExpendedor().getProducto();
@@ -46,8 +72,16 @@ public class BotonProducto extends JLabel implements MouseListener, GeneradorIma
         }
     }
 
+    /**
+     * Este metodo se utiliza cuando se deja de hacer clic al mouse
+     * @param me este es el evento del mouse
+     */
     public void mouseReleased(MouseEvent me) {;}
 
+    /**
+     * esto se utiliza cuando el mouse esta dento de del area del botón
+     * @param me este es el evento del mouse
+     */
     public void mouseEntered(MouseEvent me) {
         BotonProducto lbl = (BotonProducto) me.getComponent();
         lbl.setBackground(new Color(0x672222));
@@ -55,14 +89,27 @@ public class BotonProducto extends JLabel implements MouseListener, GeneradorIma
         Sonidos.reproducirSonido(abrirSonido,() -> {});
     }
 
+    /**
+     * Esto se utiliza cuando el mouse sale del área del botón.
+     * @param me Este es el evento del mouse.
+     */
     public void mouseExited(MouseEvent me) {
         BotonProducto lbl = (BotonProducto) me.getComponent();
         lbl.setBackground(new Color(0x000000));
         if(labelProducto!=null){labelProducto.setVisible(false);}
     }
 
+    /**
+     * establece el panel del comprador
+     * @param panelComprador este es el panel del comprador acociado al boton
+     */
     public void setPanelComprador(PanelComprador panelComprador) {this.panelComprador = panelComprador;}
 
+    /**
+     * Crea y muestra una imagen que representa el producto seleccionado en el expendedor,
+     * la imagen se carga y se posiciona de acuerdo al producto comprado
+     *
+     */
     public void impresionProducto(){
         labelProducto = switch (pExp.getComprado()){
             case COCACOLA -> GeneradorImagen.ImageLabel("src/main/java/t3/logica_interfaz/Imagenes/cocacola_90.png",40,-35,150,150);
