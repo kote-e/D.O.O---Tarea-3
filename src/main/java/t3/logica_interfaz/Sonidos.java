499ab3be6faa093bcffee50d822baa09816f0225
kote-e <mariasanmartin2023@udec.cl>
2024-06-04 18:48:14 -0400

    correcion en el javadoc de la clase Sonidos

--- src/main/java/t3/logica_interfaz/Sonidos.java
+++ src/main/java/t3/logica_interfaz/Sonidos.java
@@ -3,7 +3,17 @@ package t3.logica_interfaz;
 import javax.sound.sampled.*;
 import java.io.File;
 
+/**
+ * Esta es la interfaz Sonidos que proporciona métodos estáticos para cargar y
+ * reproducir archivos de audio
+ *
+ */
 public interface Sonidos {
+    /**
+     * Carga un archivo de sonido desde la ruta especificada y devuelve un Clip de audio
+     * @param ruta esta es la ruta del archivo a reproducir
+     * @return el sonido o null si ocurre un error
+     */
     static Clip cargarSonido(String ruta) {
         Clip clip = null;
         try {
@@ -18,6 +28,12 @@ public interface Sonidos {
         return clip;
     }
 
+    /**
+     * Reproduce el sonido del Clip especificado y ejecuta una acción al terminar
+     * la reproducción
+     * @param clip este es el clip de audio que se reproduce
+     * @param onCompletion esta es la acción que se ejecuta cuando se termina la reprodución del clip
+     */
     static void reproducirSonido(Clip clip, Runnable onCompletion) {
         if (clip != null) {
             clip.setFramePosition(0);
