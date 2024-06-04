491672ff10eefcc1bc17abd510cb006894eee784
kote-e <mariasanmartin2023@udec.cl>
2024-06-04 01:46:13 -0400

    cambios en el javadoc de la clase GeneradorImagen

--- src/main/java/t3/logica_interfaz/GeneradorImagen.java
+++ src/main/java/t3/logica_interfaz/GeneradorImagen.java
@@ -3,8 +3,20 @@ package t3.logica_interfaz;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * La interfaz GeneradorImagen proporciona métodos estáticos para cargar,
+ *redimensionar y generar etiquetas con imágenes.
+ * @author
+ */
 public interface GeneradorImagen {
-    // Cargar y redimensionar la imagen del Producto
+
+    /**
+     * Cargar y redimensionar la imagen del Producto
+     * @param nombre esta es la ruta del archivo de la imagen
+     * @param width este es el ancho deseado para la imagen redimensionada
+     * @param height esta es la altura deseada para la imagen redimensionada
+     * @return la imagen redimensionada
+     */
     static ImageIcon scaledProducto(String nombre, int width, int height){
         ImageIcon originalIcon = new ImageIcon(nombre);
         Image originalIconImage = originalIcon.getImage();
@@ -12,7 +24,15 @@ public interface GeneradorImagen {
         return new ImageIcon(scaledIconImage);
     }
 
-    //Generar una JLabel Custom para cada imagen
+    /**
+     * Generar una JLabel Custom para cada imagen
+     * @param nombre es la ruta del archivo de la imagen
+     * @param x la coordenada x donde se ubicará la etiqueta en el contenedor
+     * @param y la coordenada y donde se ubicará la etiqueta en el contenedor
+     * @param width este es el ancho de la etiqueta
+     * @param height esta es la altura de la etiqueta
+     * @return la imagen especificada
+     */
     static JLabel ImageLabel(String nombre, int x, int y, int width, int height){
         ImageIcon icon = scaledProducto(nombre, width, height);
 
