d6c291172bc3eb14cffe3d70ebd07aa4152e0811
vq0001 <vquiroga2023@udec.cl>
2024-06-05 19:40:45 -0400

    Agregar tooltip a btnAgregarProductos en PanelExpendedor

--- src/main/java/t3/logica_interfaz/PanelExpendedor.java
+++ src/main/java/t3/logica_interfaz/PanelExpendedor.java
@@ -64,6 +64,7 @@ public class PanelExpendedor extends JPanel implements GeneradorImagen{
         JButton btnAgregarProductos = new JButton(new ImageIcon("src/main/java/t3/logica_interfaz/Imagenes/reload.png"));
         btnAgregarProductos.setBounds(544,377,47,42);
         btnAgregarProductos.setBackground(new Color(0xF6AD8D));
+        btnAgregarProductos.setToolTipText("Agregar productos a expendedor");
 
         PanelExpendedor panelExpendedor = this;
         btnAgregarProductos.addMouseListener(new MouseListener(){
@@ -84,7 +85,6 @@ public class PanelExpendedor extends JPanel implements GeneradorImagen{
                 expendedor.agregarProductos(cantidad);
                 panelExpendedor.getPanelProductos().imprimirProductos();
             }
-
             @Override
             public void mousePressed(MouseEvent e) {;}
             @Override
